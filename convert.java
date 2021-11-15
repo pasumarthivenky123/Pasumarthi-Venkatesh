24b4113f689b303ceac1740abb130c1936b5ed75
pasumarthivenky123 <93004329+pasumarthivenky123@users.noreply.github.com>
2021-11-15 14:28:33 +0530

    Add files via upload

---
+++ convert.java
@@ -0,0 +1,11 @@
+ package assignment1;
+public class convert {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int i=2;
+       double b;
+       b=i/39.37;
+       System.out.println(+b);
+	}
+
+}
