8660b19acce357809bfc316065ed8cb6cfcfffdd
pasumarthivenky123 <93004329+pasumarthivenky123@users.noreply.github.com>
2021-11-16 16:15:36 +0530

    Create  Assignment 1

---
+++ add2.java
@@ -0,0 +1,10 @@
+package assignment1;
+public class add2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int d;
+		 char ch='A';
+		 d=ch+3;
+	      System.out.println(+d);
+	}
+}
